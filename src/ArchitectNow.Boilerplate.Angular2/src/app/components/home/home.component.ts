09c93f500fa3b3351b55a435b4613807ce8cbe3d
Kevin Grossnicklaus <kvgros@architectnow.net>
2016-10-14 14:12:01 -0500

    More cleanup

--- src/ArchitectNow.Boilerplate.Angular2/src/app/components/home/home.component.ts
+++ src/ArchitectNow.Boilerplate.Angular2/src/app/components/home/home.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from "@angular/core";
-import { ValuesService } from "../../services/values.service";
 
 @Component({
     template: require('./home.component.html'),
@@ -7,12 +6,11 @@ import { ValuesService } from "../../services/values.service";
 })
 export class HomeComponent implements OnInit {
 
-    values: string[];
 
-    constructor (private _valuesService: ValuesService) {}
+
+    constructor () {}
 
     ngOnInit () {
-        // this._valuesService.getValues()
-        //     .subscribe((values) => this.values = values);
+
     }
 }
